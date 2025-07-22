d3466b33876c1f71410b5b96adcf22a4ce75ad76
Felipe <41008398+StrawberryMaster@users.noreply.github.com>
2025-07-22 12:41:26 +0000

    Bumping version normally I would've yanked the old gem, but that's not working here

--- lib/wayback_machine_downloader.rb
+++ lib/wayback_machine_downloader.rb
@@ -115,7 +115,7 @@ class WaybackMachineDownloader
   include ArchiveAPI
   include SubdomainProcessor
 
-  VERSION = "2.3.11"
+  VERSION = "2.3.12"
   DEFAULT_TIMEOUT = 30
   MAX_RETRIES = 3
   RETRY_DELAY = 2
